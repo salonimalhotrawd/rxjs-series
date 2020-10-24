a621ab1e858bbee39f97fa05322e369656f57c26
Saloni Malhotra <salonim@mindfiresolutions.com>
2020-10-24 10:28:50 +0530

    Create Dynamic List for Observable List Section and navigate to its component

--- src/app/observable/list/list.component.ts
+++ src/app/observable/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './list.component.html',
@@ -6,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _router: Router) { }
+
+  public ObservableList = [
+    { 'id': 1 , 'observableName': 'From Event', 'routerName': 'from-event'},
+    { 'id': 2 , 'observableName': 'From Event', 'routerName': 'from-event'},
+    { 'id': 3 , 'observableName': 'From Event', 'routerName': 'from-event'},
+    { 'id': 4 , 'observableName': 'From Event', 'routerName': 'from-event'},
+    { 'id': 5 , 'observableName': 'From Event', 'routerName': 'from-event'},
+    { 'id': 6 , 'observableName': 'From Event', 'routerName': 'from-event'},
+    { 'id': 7 , 'observableName': 'From Event', 'routerName': 'from-event'}
+  ];
 
   ngOnInit() {
+  }
+
+  public navigateToRoute(event): void {
+    const url = '../observable/' + event;
+    this._router.navigate([url]);
   }
 
 }
