4d170571ba7885372cde85d09d1f9778efa2d95c
Saloni Malhotra <salonim@mindfiresolutions.com>
2020-10-25 14:42:14 +0530

    Integerate take functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ import { MapComponent } from './observable/map/map.component';
 import { PluckComponent } from './observable/pluck/pluck.component';
 import { FilterComponent } from './observable/filter/filter.component';
 import { TapComponent } from './observable/tap/tap.component';
+import { TakeComponent } from './observable/take/take.component';
 
 const routes: Routes = [
   { path: 'promise', component: PromiseComponent },
@@ -32,6 +33,7 @@ const routes: Routes = [
         { path: 'pluck', component: PluckComponent },
         { path: 'filter', component: FilterComponent },
         { path: 'tap', component: TapComponent },
+        { path: 'take', component: TakeComponent },
       ]
   },
   { path: '**', redirectTo: 'promise' }
